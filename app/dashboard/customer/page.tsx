34be7927f68b82fd75599d6354ec8fd9803394db
ibn <officialibn001@gmail.com>
2023-11-04 15:04:01 +0100

    Completed chapter 4

---
+++ app/dashboard/customer/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CustomersPage = () => {
+  return (
+    <div>
+        <p>
+            CustomersPage
+        </p>
+    </div>
+  )
+}
+
+export default CustomersPage
