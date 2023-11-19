d9db85f7f5a8e06cf9d4b224f70027303d51e048
ibn <officialibn001@gmail.com>
2023-11-19 17:16:43 +0100

    Clean Project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
-import '@/app/ui/global.css'
-import { inter } from './ui/fonts';
+import "@/app/global.css";
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body>{children}</body>
+		</html>
+	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,51 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      gridTemplateColumns: {
-        '13': 'repeat(13, minmax(0, 1fr))',
-      },
-      colors: {
-        blue: {
-          400: '#2589FE',
-          500: '#0070F3',
-          600: '#2F6FEB',
-        },
-      },
-    },
-    keyframes: {
-      shimmer: {
-        '100%': {
-          transform: 'translateX(100%)',
-        },
-      },
-    },
-  },
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				"electric-violet": {
+					"50": "#f5f3ff",
+					"100": "#ede9fe",
+					"200": "#ddd6fe",
+					"300": "#c4b5fd",
+					"400": "#a78afb",
+					"500": "#8b5bf7",
+					"600": "#7c3aee",
+					"700": "#6d27da",
+					"800": "#5b20b7",
+					"900": "#4c1c96",
+					"950": "#2e1065",
+				},
+				bunker: {
+					"50": "#f1f5fe",
+					"100": "#e2eafc",
+					"200": "#bed2f9",
+					"300": "#85adf4",
+					"400": "#4483ec",
+					"500": "#1c64db",
+					"600": "#0f4aba",
+					"700": "#0d3b97",
+					"800": "#0f347d",
+					"900": "#122d68",
+					"950": "#030711",
+				},
+			},
+		},
+		keyframes: {
+			shimmer: {
+				"100%": {
+					transform: "translateX(100%)",
+				},
+			},
+		},
+	},
 
-  plugins: [require('@tailwindcss/forms')],
+	plugins: [require("@tailwindcss/forms")],
 };
 export default config;
