628fa10b8982756b0a0705ae4cb436c52c469fbf
ibn <officialibn001@gmail.com>
2023-11-19 17:36:09 +0100

    Made some resets

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
-import Image from "next/image";
-export default function Page() {
+const Page = () => {
 	return (
 		<main>
 			<h1 className='text3xl'>Hello World!!</h1>
 		</main>
 	);
-}
+};
+
+export default Page;
