42ab21a471db1dacff76da1a56687661b07472f5
ibn <officialibn001@gmail.com>
2023-11-28 08:02:13 +0100

    creatde new account and its styles component

---
+++ app/components/NewAccount.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "@/app/components/styles/NewAccount.css";
+
+const NewAccount = () => {
+	return <div className='newAccount'></div>;
+};
+
+export default NewAccount;
