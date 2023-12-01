4dd121fa9b1096267138845f30acb5624091f296
ibn <officialibn001@gmail.com>
2023-12-01 14:31:29 +0100

    added placeholder user data

--- app/lib/placeholder_data.ts
+++ app/lib/placeholder_data.ts
@@ -1,4 +1,4 @@
-import { InvestmentCardData } from "@/typings.t";
+import { InvestmentCardData, UserData } from "@/typings.t";
 
 const dummyInvestmentData: InvestmentCardData = {
 	totalEarning: 50000,
@@ -6,4 +6,68 @@ const dummyInvestmentData: InvestmentCardData = {
 	referralIncome: 2000,
 	totalInvested: 30000,
 	activeInvestmentIncome: 25000,
+};
+
+// Dummy data for testing
+export const dummyUserData: UserData = {
+	name: "John Doe",
+	email: "johndoe@gmail.com",
+	password: "12345",
+	username: "john_doe",
+	address: {
+		houseNumber: "123",
+		street: "Main Street",
+		state: "California",
+		country: "USA",
+		postcode: "12345",
+	},
+	accounts: {
+		depositAccount: {
+			accountName: "Savings",
+			accountNumber: "1234567890",
+			bankName: "Bank of Example",
+		},
+		withdrawalAccounts: [
+			{
+				accountName: "Checking",
+				accountNumber: "0987654321",
+				bankName: "Example Bank",
+			},
+		],
+	},
+	deposit: [
+		{
+			amount: 1000,
+			timestamp: new Date("2023-01-01"),
+		},
+		{
+			amount: 500,
+			timestamp: new Date("2023-02-01"),
+		},
+	],
+	income: 5000,
+	withdrawalCash: {
+		totalWithdrawal: 2000,
+		availableWithdrawal: 1000,
+	},
+	activeInvestments: [
+		{
+			name: "Stock ABC",
+			amount: 2000,
+			time: new Date("2023-03-01"),
+			percentage: 2,
+			ROI: 40,
+			withdrawable: new Date("2023-04-01"),
+			close: new Date("2023-05-01"),
+		},
+	],
+	closedInvestments: [
+		{
+			name: "Crypto XYZ",
+			amount: 1500,
+			ROI: 30,
+			timeOpened: new Date("2022-12-01"),
+			timeCompleted: new Date("2023-01-01"),
+		},
+	],
 };
--- typings.t.ts
+++ typings.t.ts
@@ -5,3 +5,59 @@ export type InvestmentCardData = {
 	totalInvested: number;
 	activeInvestmentIncome: number;
 };
+
+// Define types for the user schema
+type Address = {
+	houseNumber: string;
+	street: string;
+	state: string;
+	country: string;
+	postcode: string;
+};
+
+type Account = {
+	accountName: string;
+	accountNumber: string;
+	bankName: string;
+};
+
+type ActiveInvestment = {
+	name: string;
+	amount: number;
+	time: Date;
+	percentage: number;
+	ROI: number;
+	withdrawable: Date;
+	close: Date;
+};
+
+type ClosedInvestment = {
+	name: string;
+	amount: number;
+	ROI: number;
+	timeOpened: Date;
+	timeCompleted: Date;
+};
+
+export type UserData = {
+	name: string;
+	email: string;
+	password: string;
+	username: string;
+	address: Address;
+	accounts: {
+		depositAccount: Account;
+		withdrawalAccounts: Account[];
+	};
+	deposit: {
+		amount: number;
+		timestamp: Date;
+	}[];
+	income: number;
+	withdrawalCash: {
+		totalWithdrawal: number;
+		availableWithdrawal: number;
+	};
+	activeInvestments: ActiveInvestment[];
+	closedInvestments: ClosedInvestment[];
+};
