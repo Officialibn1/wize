92bcaca589312326d6f9e38ca5ff3ea1428493d5
ibn <officialibn001@gmail.com>
2023-12-02 22:10:08 +0100

    Product stock complete

--- app/components/ui/PorpularStock.tsx
+++ app/components/ui/PorpularStock.tsx
@@ -9,14 +9,27 @@ const PorpularStock = ({ stock }: { stock: Stock }) => {
 			<div className='stockText'>
 				<h1>{stock.name}</h1>
 
-				<p>{stock.percentageChange}% Profit</p>
+				<p
+					className={
+						stock.percentageChange >= 0 ? "text-green-400" : "text-red-400"
+					}>
+					{stock.percentageChange}%{" "}
+					{stock.percentageChange >= 0 ? "Profit" : "Loss"}
+				</p>
 			</div>
 
 			<div className='stockPrice'>
 				<h3>${stock.price}</h3>
 
-				<span>
-					<IoIosArrowDown />
+				<span
+					className={
+						stock.percentageChange >= 0 ? "bg-green-400/25" : "bg-red-400/25"
+					}>
+					{stock.percentageChange >= 0 ? (
+						<IoIosArrowUp className='text-green-500' />
+					) : (
+						<IoIosArrowDown className='text-red-500' />
+					)}
 				</span>
 			</div>
 		</div>
