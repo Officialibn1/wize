2046c1c17d6b8a2f6d1267656a0306ec4e442d78
ibn <officialibn001@gmail.com>
2023-12-02 23:37:56 +0100

    Service section text

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -13,6 +13,26 @@ const ServicesPage = () => {
 			{/* Hero banner */}
 			<OurServiceHeader />
 
+			<div className='teamAproach'>
+				<div>
+					<p>
+						With our team approach, our goal is to identify new ways to{" "}
+						<b className=' font-semibold'>
+							increase performance, reduce cost and manage risk.
+						</b>{" "}
+						With TradeStation Institutional Services, your firm will be working
+						with an experienced and professional institutional team committed to
+						your relationship.
+					</p>
+					<br />
+					{/* <br /> */}
+					<p>
+						Review our services below to see how we can help you to achieve your
+						goals.
+					</p>
+				</div>
+			</div>
+
 			<Footer />
 		</div>
 	);
