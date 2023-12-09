f5d3840d6535568d66654ff6538c2c9a3897b1c8
ibn <officialibn001@gmail.com>
2023-12-09 12:02:09 +0100

    added sharp dependency

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,12 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/dashboard"] };
+export const config = {
+	matcher: [
+		"/dashboard",
+		"/dashboard/wallet",
+		"/dashboard/transactions",
+		"/dashboard/new_investment",
+		"/dashboard/profile",
+		"/dashboard/settings",
+	],
+};
